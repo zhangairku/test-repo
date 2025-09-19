74b5f4f64fa699e90f7fd053e9c2c60eac0eb9a6
zhangxin <v_zhangxin79@baidu.com>
2025-09-19 09:39:04 +0800

    v1

--- core.go
+++ core.go
@@ -1,11 +1,13 @@
-package test_repo
+package repo
 
 import (
-	"fmt"
-	"math/rand/v2"
+	"github.com/google/uuid"
 )
 
 func Core() string {
-	i := rand.N(10000)
-	return fmt.Sprintf("Core: %d", i)
+	v7, err := uuid.NewV7()
+	if err != nil {
+		panic(err)
+	}
+	return v7.String()
 }
